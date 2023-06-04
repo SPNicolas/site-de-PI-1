0c5a5a32b9b2aaace0a73897bdaa9a2c4f74a934
matheushodecker <matheus.hodecker.santos@gmail.com>
2023-06-04 16:55:07 -0300

    conteudo adicionado

--- site de PI/script.js
+++ site de PI/script.js
@@ -1,9 +1,9 @@
-function abrirMenu(){
-     let barraMenuAberto = document.getElementById('barra-menu');
-    
-     if(barraMenuAberto.style.width == "0px"){
-        barraMenuAberto.style.width = "180px";
-     }else{
-        barraMenuAberto.style.width = "0px";
-    }
+function abrirMenu() {
+   let barraMenuAberto = document.getElementById('barra-menu');
+
+   if (barraMenuAberto.style.width == "0px") {
+      barraMenuAberto.style.width = "180px";
+   } else {
+      barraMenuAberto.style.width = "0px";
+   }
 }
